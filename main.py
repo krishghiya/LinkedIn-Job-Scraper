d7d72b2e7d21c97fa77877949da1f679eacecd1c
Krish Ghiya <kghiya2@gmail.com>
2021-01-22 19:59:28 -0800

    Updated to current website formatting

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ PASSWORD = ''
 
 # Keep variable values lowercase to avoid conflicts
 
-TITLE = "software"  # Title of job you are looking for
+TITLE = ""  # Title of job you are looking for
 LOCATION = "United States"  # City, State, or Country
-EXCLUDE_KEYWORDS = ["years of", "years exp", "+ year", "of experience"]  # Exclude jobs with these words in description
-EXCLUDE_JOBS = ["intern", "full"]  # Exclude jobs with these in the title
-INCLUDE_KEYWORDS = ["ba ", "undergrad", " b.", "bs", "java", "python"]  # Include jobs with these words in description
+EXCLUDE_KEYWORDS = []  # Exclude jobs with these words in description
+EXCLUDE_JOBS = []  # Exclude jobs with these in the title
+INCLUDE_KEYWORDS = [[], []]  # Include jobs with these words in description
 INCLUDE_JOBS = []  # Include jobs whose title contains these words
 JOB_TYPES = ["fulltime"]  # One of: internship, fulltime, part-time
 PAGES = 40  # Number of jobs (PAGES * 25) to scan. Max 40
 SITE = "https://www.linkedin.com/"
-DRIVER = webdriver.Edge(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedgedriver.exe')
+DRIVER = webdriver.Chrome(r'C:\Program Files (x86)\Google\chromedriver.exe')
 
 companies = OrderedDict()
 
@@ -47,7 +47,8 @@ DRIVER.get(SITE + "jobs")
 DRIVER.add_cookie({
     'name': 'li_at',
     # To avoid crashes use a new value every session
-    'value': "",
+    'value': "AQEDASWTK4wEgkUkAAABdy1TwmIAAAF3UWBGYk4AJlUlYsB-UWGZSmwlR7TcmRGCxt9R4k88BtnGkYson1"
+             "eQTzAahkzwDy6_oAzHESO3TvigQjVodQpe8WD_I4637DKQFoCHotZEaYD37h8uNVWYdu3s",
     'domain': '.linkedin.com'
 })
 
@@ -67,13 +68,13 @@ search = search_boxes[1]
 search.clear()
 search.send_keys(LOCATION)
 search.send_keys(Keys.RETURN)
-job_type = get_elem("data-test-facet-name", value="jobType")
+job_type = get_elem("aria-controls", value="job-type-facet-values")
 job_type.click()
 
 for job in JOB_TYPES:
-    get_elem("for", value="jobType-" + job[0].upper(), lookup=job_type).click()
+    get_elem("for", value="jobType-" + job[0].upper(), lookup=DRIVER).click()
 
-get_elem("data-control-name", value="filter_pill_apply", lookup=job_type).click()
+get_elem("class", value="msg-overlay-bubble-header", compare="=", lookup=DRIVER).click()
 url = DRIVER.current_url
 page = 1
 no_results = []
@@ -81,8 +82,8 @@ time.sleep(1)
 
 while not no_results and page <= PAGES:
     try:
-        jobs = get_elem("data-test-search-two-pane-search-result-item", value="true", multiple=True)
-    except TimeoutException:
+        jobs = get_elem("data-occludable-entity-urn", compare="", multiple=True)
+    except:
         print("Site crashed, saving data...")
         break
 
@@ -92,6 +93,7 @@ while not no_results and page <= PAGES:
     try:
         for i in jobs:
             DRIVER.execute_script("arguments[0].scrollIntoView();", i)
+            time.sleep(0.5)
             job_link = get_elem("href", tag="a", compare="", lookup=i)
             job_link.click()
             job_link = job_link.get_attribute("href")
@@ -105,13 +107,14 @@ while not no_results and page <= PAGES:
             description = get_elem("id", value="job-details").text.lower()
 
             if not any(s in description for s in EXCLUDE_KEYWORDS) and \
-                    any(s in description for s in INCLUDE_KEYWORDS):
+                    all(any(s in description for s in arr) for arr in INCLUDE_KEYWORDS):
                 job_pair = job_info[0] + "--" + job_info[2]
                 if job_pair not in companies:
                     count += 1
                     companies.update({job_pair: job_link})
 
         print("Page " + str(page) + " complete. Found " + str(count) + " jobs.")
+        time.sleep(1)
         DRIVER.get(url + "&start=" + str(page * 25))
         page += 1
         no_results = DRIVER.find_elements_by_class_name("jobs-search-no-results__image")
